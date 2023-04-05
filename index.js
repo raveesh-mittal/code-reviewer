b29ef0902012ff74d0be69e3292b21dd56703d77
Raveesh Mittal <raveesh.mittal@nutanix.com>
2023-04-05 18:04:46 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const slackToken = core.getInput('slack-token');
+    const slackChannel = core.getInput('slack-channel');
+    const githubToken = core.getInput('github-token');
+
+    console.log(`Running execution: `, slackChannel);
+    const octokit = github.getOctokit(githubToken); 
+    const response = await axios.post('https://slack.com/api/chat.postMessage', {
+      "channel": slackChannel,
+      "text": "Test Message"
+    }, {
+      headers: {
+        Authorization: `Bearer ${slackToken}`
+      }
+    });
+    if (response.status === 200) {
+      core.setOutput("success", true);
+    }
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    console.log('err: ', error);
+    // core.setFailed(error.message);
+  }
 }
+
+run();
