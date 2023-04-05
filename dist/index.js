962b14fd2308f7f0499fdd4b58e5182182a70ba7
Raveesh Mittal <raveesh.mittal@nutanix.com>
2023-04-05 18:38:18 +0530

    added slack response

--- dist/index.js
+++ dist/index.js
@@ -16285,22 +16285,15 @@ const axios = __nccwpck_require__(9923);
 
 async function run() {
   try {
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-    core.setOutput('success', false);
-
     const slackToken = core.getInput('slack-token');
     const slackChannel = core.getInput('slack-channel');
-    const githubToken = core.getInput('github-token');
+    core.setOutput('success', false);
 
-    console.log(`Running execution 2: `, slackChannel);
-    const octokit = github.getOctokit(githubToken);
     const response = await axios.post(
       'https://slack.com/api/chat.postMessage',
       {
         channel: slackChannel,
-        text: 'Test Message'
+        text: 'PR has been opened'
       },
       {
         headers: {
@@ -16308,10 +16301,31 @@ async function run() {
         }
       }
     );
-    console.log('slackResponse: ', response);
     if (response.status === 200) {
       core.setOutput('success', true);
     }
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context, undefined, 2);
+    const prLink = github.context.payload.pull_request.self.href;
+
+    const reposPath = prLink.split('.com')[1];
+    console.log(`The event payload: ${payload}`);
+    console.log('reposPath: ', reposPath);
+
+    const githubToken = core.getInput('github-token');
+    const octokit = github.getOctokit(githubToken);
+    const gitResponse = await octokit.request(`GET ${reposPath}`, {
+      owner: 'OWNER',
+      repo: 'REPO',
+      pull_number: 'PULL_NUMBER',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(
+      `gitApi Response: ${JSON.stringify(gitResponse, undefined, 2)}`
+    );
   } catch (error) {
     console.log('err: ', error);
     core.setOutput('success', false);
